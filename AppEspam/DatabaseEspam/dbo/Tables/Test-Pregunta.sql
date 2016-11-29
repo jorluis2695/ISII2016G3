c16ef6825ca81c8fed062ec449a42322759d1995
jguilindro <julio.guilindro@gmail.com>
2016-11-29 05:10:38 -0500

    Rediseño de Proyecto usando VS15

---
+++ AppEspam/DatabaseEspam/dbo/Tables/Test-Pregunta.sql
@@ -0,0 +1,16 @@
+﻿CREATE TABLE [dbo].[Test-Pregunta](
+	[Id_test] [int] NOT NULL,
+	[Id_pregunta] [int] NOT NULL
+) ON [PRIMARY]
+GO
+ALTER TABLE [dbo].[Test-Pregunta]  WITH CHECK ADD  CONSTRAINT [fk_pregunta] FOREIGN KEY([Id_pregunta])
+REFERENCES [dbo].[Preguntas] ([Id])
+GO
+
+ALTER TABLE [dbo].[Test-Pregunta] CHECK CONSTRAINT [fk_pregunta]
+GO
+ALTER TABLE [dbo].[Test-Pregunta]  WITH CHECK ADD  CONSTRAINT [fk_test] FOREIGN KEY([Id_test])
+REFERENCES [dbo].[Test] ([TestID])
+GO
+
+ALTER TABLE [dbo].[Test-Pregunta] CHECK CONSTRAINT [fk_test]
