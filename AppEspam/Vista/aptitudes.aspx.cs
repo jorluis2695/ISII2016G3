1527d9628a39c3210fd9253564c12db5d8de54e3
jguilindro <julio.guilindro@gmail.com>
2017-01-06 07:48:18 -0500

    aptitudes autogenerar

--- AppEspam/Vista/aptitudes.aspx.cs
+++ AppEspam/Vista/aptitudes.aspx.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Data;
+using System.Data.SqlClient;
 
 namespace Vista
 {
@@ -11,7 +13,27 @@ namespace Vista
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            using (SqlConnection con = new SqlConnection("Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=DatabaseEspam;Integrated Security=True;Persist Security Info=False;Pooling=False;MultipleActiveResultSets=False;Connect Timeout=60;Encrypt=False;TrustServerCertificate=True"))
+            {
+                SqlCommand cmd = new SqlCommand();
+                SqlDataReader reader;
+                cmd.CommandText = "GetTest";
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Connection = con;
+                con.Open();
+                reader = cmd.ExecuteReader();
+                reader.Read();
 
-        }
+                try
+                {
+                    TestEspacio.InnerHtml = "<div class=\"form-group\" > < label class=\"col-sm-2 control-label\" >" + reader.GetString(0) + "</label> < div class=\"col-sm-10\"> <div class=\"radio\"><label> <input type=\"radio\" name =\"optionsRadios\" id =\"optionsRadios1\" value =\"\" checked>" + reader.GetString(1) + "</label></div><div class=\"radio\"><label><input type=\"radio\" name =\"optionsRadios\" id =\"optionsRadios2\" value =\"\">" + reader.GetString(2) + "</label></div><div class=\"radio\"> <label><input type=\"radio\" name =\"optionsRadios\" id =\"optionsRadios3\" value =\"\" >" + reader.GetString(3) + "</label> </div><div class=\"radio\"> <label><input type=\"radio\" name =\"optionsRadios\" id =\"optionsRadios4\" value =\"\" >" + reader.GetString(4) + " </label> </div></div></div>";
+                    con.Close();
+                }
+                catch
+                {
+                    string display = "Error";
+                    ClientScript.RegisterStartupScript(this.GetType(), "No se encontraron tests dentro de la Base de Datos", "alert('" + display + "');", true);
+                }
+            }
     }
 }
