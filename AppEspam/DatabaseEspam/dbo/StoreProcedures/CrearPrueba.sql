791e142f953be9966a8ca5f075cd3e9f87abfcec
jorluis2695 <jorluis2695@hotmail.com>
2017-01-06 05:13:22 -0500

    Procedures de guardar resultados de pruebas
    .

---
+++ AppEspam/DatabaseEspam/dbo/StoreProcedures/CrearPrueba.sql
@@ -0,0 +1,8 @@
+﻿CREATE PROCEDURE [dbo].[CrearPrueba]
+	@EstudianteID int,
+	@ConvocatoriaTestID int,
+	@fecha date
+AS
+	INSERT INTO Prueba (ConvocatoriaTestID, Id_estudiante, Fecha)
+	VALUES (@ConvocatoriaTestID, @EstudianteID, @fecha)
+RETURN 0
---
+++ AppEspam/DatabaseEspam/dbo/StoreProcedures/GuardarRespuestaPregunta.sql
@@ -0,0 +1,8 @@
+﻿CREATE PROCEDURE [dbo].[GuardarRespuestaPregunta]
+	@PruebaID int,
+	@PreguntaID int,
+	@opcion int
+AS
+	INSERT INTO PregPregunta_estudiante
+	VALUES (@PruebaID, @PreguntaID, @opcion)
+RETURN 0
