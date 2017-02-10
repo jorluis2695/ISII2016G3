7668ef1aaae39caf8e02aa32f5ff6a8510381ba4
jorluis2695 <jorluis2695@hotmail.com>
2017-02-10 03:02:34 -0500

    Pequenos cambios

---
+++ DatabaseEspam/DatabaseEspam/dbo/Tables/Administrador.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[Administrador]
+(
+	[AdminID] INT NOT NULL PRIMARY KEY, 
+    [Nombres] NVARCHAR(50) NULL, 
+    [Apellidos] NVARCHAR(50) NULL, 
+    [Cedula] NVARCHAR(50) NULL
+)
